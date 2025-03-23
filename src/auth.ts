6c0c9c0b45fb5ee9dac8ef8c3ea36767c009705d
Azi-jsProger <aziorozbayev2008@gmail.com>
2025-03-23 14:51:27 +0600

    next auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,17 @@
 import CredentialsProvider from "next-auth/providers/credentials"
-import { NextAuthOptions } from "next-auth";
-import { AuthenticatedFields, CustomJWTType, JWTUser, ReponseUserToken } from "./types/auth";
-import { refreshAccessToken, requestTokenAuthorize } from "@/service/auth";
+import {NextAuthOptions} from "next-auth";
+import {AuthenticatedFields, CustomJWTType, JWTUser, ReponseUserToken} from "./types/auth";
+
+import {refreshAccessToken, requestTokenAuthorize} from "@/service/auth";
+
 
 export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
             name: "AuthCredentials",
             credentials: {
-                email: { label: "Email", type: "email", placeholder: "Email..." },
-                password: { label: "Password", type: "password", placeholder: "Password..." }
+                email: {label: "Email", type: "email", placeholder: "Email..."},
+                password: {label: "Password", type: "password", placeholder: "Password..."}
             },
             async authorize(credentials, req) {
                 if (!credentials?.email || !credentials.password) {
@@ -18,81 +20,80 @@ export const authOptions: NextAuthOptions = {
                 const data: AuthenticatedFields = {
                     identifier: credentials.email,
                     password: credentials.password
-                };
-
+                }
                 try {
                     const resData = await requestTokenAuthorize<ReponseUserToken>(data);
-                    const { accessToken, refreshToken, expireIn } = resData;
-
+                    const {accessToken, refreshToken, expireIn} = resData;
+                    console.log('j3orihvbr');
+                    
                     const responseUserDetails = await fetch("http://localhost:8080/api/v1/auth/user/details", {
                         method: "GET",
                         headers: {
                             "Content-Type": "application/json",
                             "Authorization": `Bearer ${accessToken}`
                         }
-                    });
-
-                    if (!responseUserDetails.ok) {
-                        throw new Error("Ошибка получения данных пользователя");
-                    }
+                    })
 
                     const userDetails = await responseUserDetails.json();
-                    console.log("User Details:", userDetails);
+                    console.log(userDetails,'user Detail')
 
-                    const { username, email, id } = userDetails;
+                    const {username, email, id} = userDetails
+
 
                     return {
                         id: String(id),
-                        username,
-                        email,
-                        accessToken,
-                        refreshToken,
-                        accessTokenExpires: Date.now() + expireIn * 1000, 
-                        details: userDetails
-                    } as JWTUser;
+                        username: username,
+                        email: email,
+                        accessToken: accessToken,
+                        refreshToken: refreshToken,
+                        accessTokenExpires: expireIn
+                    } as JWTUser
 
                 } catch (error) {
-                    console.error("Authorization error:", error);
+                    console.error('Authorization error:', error);
                     return null;
                 }
             },
         })
     ],
-    session: { strategy: "jwt" },
+    session: {strategy: 'jwt'},
     pages: {
         signIn: "/auth/signin"
     },
     callbacks: {
-        async jwt({ token, user }) {
+        async jwt({token, user}) {
+            const jwtUser = user as JWTUser
+            const jwtToken = token as CustomJWTType
             if (user) {
+                console.log("HEllo wolrld", jwtUser)
                 return {
-                    accessToken: user.accessToken,
-                    refreshToken: user.refreshToken,
-                    accessTokenExpires: user.accessTokenExpires,
-                    details: user.details,
+                    accessToken: jwtUser.accessToken,
+                    refreshToken: jwtUser.refreshToken,
+                    accessTokenExpires: jwtUser.accessTokenExpires,
                     user
-                } as CustomJWTType;
+                };
             }
 
-            if (Date.now() < token.accessTokenExpires) {
-                console.log("Token is still valid");
+            console.log(Date.now(), jwtToken)
+            if (Date.now() < jwtToken.accessTokenExpires) {
+                console.log("Get token")
                 return token;
             }
-
-            const generatedToken = await refreshAccessToken(token);
-            console.log("Generated new token:", generatedToken);
+            const generatedToken = await refreshAccessToken(jwtToken);
+            console.log("Generated token", generatedToken)
             return generatedToken;
         },
 
-        async session({ session, token }) {
+
+        async session({session, token}) {
             session.user = {
                 ...session.user,
                 accessToken: token.accessToken,
-                refreshToken: token.refreshToken,
-                details: token.details
-            } as JWTUser;
+                refreshToken: token.refreshToken
+            } as JWTUser
 
             return session;
         }
+
     }
-};
+}
